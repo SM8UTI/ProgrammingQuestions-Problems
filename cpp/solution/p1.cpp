ac5ff4cd5b646aa484c0a2758e4bd80a2799a439
Smruti Ranjan Nayak <smrutiranjannayak5403@gmail.com>
2022-01-13 21:27:48 +0530

    update p1.cpp

--- cpp/solution/p1.cpp
+++ cpp/solution/p1.cpp
@@ -1,10 +1,10 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 
 int main(){
-    int row,col;
-    cin>>row>>col;
+	int row,col;
+	cin>>row>>col;
 
     for(int i = 1;i<=row;i++){
         for(int j = 1;j<=col;j++){
@@ -12,4 +12,21 @@ int main(){
         }
         cout<<endl;
     }
+    cout<<endl;
+	for(int i = 1;i<=row;i++){
+		for(int j=1;j<=col;j++){
+			if(i == 1 || i == row){
+				cout<<"X ";
+			}else if(j == 1 || j == col){
+				cout<<"X ";
+			}
+
+			else{
+				cout<<"  ";
+			}
+		}
+		cout<<endl;
+	}
+
 }
+
