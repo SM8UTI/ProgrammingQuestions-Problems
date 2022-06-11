bcc75e72e24c9921971417250c951732556ac8d4
Smruti Ranjan Nayak <56423859+SM8UTI@users.noreply.github.com>
2022-06-11 17:34:25 +0530

    rgg

--- cpp/solution/half_pyramid.cpp
+++ cpp/solution/half_pyramid.cpp
@@ -1,12 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Smruti Ranjan Nayk
+
+
+
 
 int main(){
-	int row;
-	cin>>row;
+	int r;
+	cin>>r;
 
-	for(int i = 1;i<=row;i++){
+	for(int i = 1;i<=r;i++){
 		for(int j = 1;j<=i;j++){
 			cout<<"X ";
 		}
